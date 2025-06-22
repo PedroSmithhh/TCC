d7df804d91495b76274d5e4235798cb5b69d4e52
Gabriel Cassola <cassola.gabriel@gmail.com>
2025-06-22 18:35:22 -0300

    V0 do frontend

---
+++ frontend/frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <MapaBauru />
+</template>
+
+<script>
+import MapaBauru from './components/MapaBauru.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MapaBauru
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
